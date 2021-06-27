06de530b615072349b27ce4c2cac4c43c2ce52a0
warren yun <xxwarrenhdxx@gmail.com>
2021-06-27 05:12:14 -0400

    Finish landing page

--- components/Landing.js
+++ components/Landing.js
@@ -12,6 +12,9 @@ const Landing = () => {
                 <h2 className={landingStyles.text}>
                     a high schooler with a passion for programming and a strong addiction to learning new things
                 </h2>
+                <div className={landingStyles.pfpContainer}>
+                    <img src="/warren.png" className={landingStyles.pfp} alt="warren" />
+                </div>
             </div>
             <div className={landingStyles.socialMediaWrapper}>
                 <a href="https://www.instagram.com/nebudev14/" target="_blank">
