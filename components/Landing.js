b3ff10f82bc58e50c2373924cc99dd232a0926e5
warren yun <xxwarrenhdxx@gmail.com>
2021-06-26 20:34:44 -0400

    Remove pfp and made title simpler

--- components/Landing.js
+++ components/Landing.js
@@ -3,16 +3,9 @@ import landingStyles from '../styles/Landing.module.css';
 const Landing = () => {
     return (
         <div className={landingStyles.container}>
-            <div className={landingStyles.titleDiv}>
-                <div className={landingStyles.title}>
-                    <strong>
-                        <span className={landingStyles.firstName}>warren</span> yun
-                    </strong>
-                </div>
-                <div>
-                    <img src="/warren.png" alt="image of warren" />
-                </div>
-            </div>
+            <span className={landingStyles.title}>
+                <span className={landingStyles.firstName}>warren</span> yun
+            </span>
         </div>
     );
 }
