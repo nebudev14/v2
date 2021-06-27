186cbc6d1950232254786c4c36a95045c1a5ab05
warren yun <xxwarrenhdxx@gmail.com>
2021-06-27 14:05:33 -0400

    Add box shadow to nav hover

--- components/Nav.js
+++ components/Nav.js
@@ -4,19 +4,19 @@ const Nav = () => {
     return (        
         <ul className={navStyles.navList}>
             <li className={navStyles.navElement}>
-                <a href="/projects" className={navStyles.link}>projects</a>
+                <a href="/projects" className={navStyles.link}>Projects</a>
             </li>
             <li className={navStyles.navElement}>
-                <a href="https://scrapbook.hackclub.com/WarrenYun/" className={navStyles.link} target="_blank">scrapbook</a>
+                <a href="https://scrapbook.hackclub.com/WarrenYun/" className={navStyles.link} target="_blank">Scrapbook</a>
             </li>
             <li className={navStyles.navElement}>
-                <a href="/" className={navStyles.link}>home</a>
+                <a href="/" className={navStyles.link}>Home</a>
             </li>
             <li className={navStyles.navElement}>
-                <a href="/blog" className={navStyles.link}>blog</a>
+                <a href="/blog" className={navStyles.link}>Blog</a>
             </li>
             <li className={navStyles.navElement}>
-                <a href="https://www.polywork.com/nebudev14" className={navStyles.link} target="_blank">timeline</a>
+                <a href="https://www.polywork.com/nebudev14" className={navStyles.link} target="_blank">Timeline</a>
             </li>
         </ul>
         
