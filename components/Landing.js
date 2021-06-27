c6518001dbbb7919e0dfeadd6df81c1434542eb0
warren yun <xxwarrenhdxx@gmail.com>
2021-06-27 00:07:16 -0400

    Add social media icons

--- components/Landing.js
+++ components/Landing.js
@@ -1,11 +1,24 @@
 import landingStyles from '../styles/Landing.module.css';
 
 const Landing = () => {
+    
     return (
         <div className={landingStyles.container}>
             <span className={landingStyles.title}>
                 <span className={landingStyles.firstName}>warren</span> yun
+                <div className={landingStyles.socialMediaWrapper}>
+                    <a href="https://webring.hackclub.com/">
+                        <img src="/instagram.png" className={landingStyles.logo} />
+                    </a>
+                    <a href="https://webring.hackclub.com/">
+                        <img src="/twitter.png" className={landingStyles.logo} />
+                    </a>
+                    <a href="https://webring.hackclub.com/">
+                        <img src="/github.png" className={landingStyles.logo} />
+                    </a>
+              </div>
             </span>
+            
         </div>
     );
 }
