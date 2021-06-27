b0f1c44d8df4eab31006c3dd0dbcf21c98c8f6e5
warren yun <xxwarrenhdxx@gmail.com>
2021-06-27 00:10:07 -0400

    Add social media link

--- components/Landing.js
+++ components/Landing.js
@@ -7,13 +7,13 @@ const Landing = () => {
             <span className={landingStyles.title}>
                 <span className={landingStyles.firstName}>warren</span> yun
                 <div className={landingStyles.socialMediaWrapper}>
-                    <a href="https://webring.hackclub.com/">
+                    <a href="https://www.instagram.com/nebudev14/" target="_blank">
                         <img src="/instagram.png" className={landingStyles.logo} />
                     </a>
-                    <a href="https://webring.hackclub.com/">
+                    <a href="https://twitter.com/nebudev14" target="_blank">
                         <img src="/twitter.png" className={landingStyles.logo} />
                     </a>
-                    <a href="https://webring.hackclub.com/">
+                    <a href="https://github.com/NebuDev14" target="_blank">
                         <img src="/github.png" className={landingStyles.logo} />
                     </a>
               </div>
