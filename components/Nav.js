c303e96888320b1e0f140916b478a173305ef243
warren yun <xxwarrenhdxx@gmail.com>
2021-06-27 04:23:35 -0400

    Complete navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,25 @@
 import navStyles from '../styles/Nav.module.css';
 
 const Nav = () => {
-    return (
-        <nav>
-            <ul className={navStyles.navList}>
-                <li className={navStyles.navElement}>
-                    <a href="/projects">projects</a>
-                </li>
-                <li className={navStyles.navElement}>
-                    <a href="https://scrapbook.hackclub.com/WarrenYun/">scrapbook</a>
-                </li>
-            </ul>
-        </nav>
+    return (        
+        <ul className={navStyles.navList}>
+            <li className={navStyles.navElement}>
+                <a href="/projects">projects</a>
+            </li>
+            <li className={navStyles.navElement}>
+                <a href="https://scrapbook.hackclub.com/WarrenYun/">scrapbook</a>
+            </li>
+            <li className={navStyles.navElement}>
+                <a href="/">home</a>
+            </li>
+            <li className={navStyles.navElement}>
+                <a href="https://scrapbook.hackclub.com/WarrenYun/">blog</a>
+            </li>
+            <li className={navStyles.navElement}>
+                <a href="https://scrapbook.hackclub.com/WarrenYun/">timeline</a>
+            </li>
+        </ul>
+        
     );
 }
 
