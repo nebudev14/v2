d94884deca510ee475797e28e554c0f9471a077b
warren yun <xxwarrenhdxx@gmail.com>
2021-06-27 21:59:57 -0400

    Add card description for projects

--- components/Project.js
+++ components/Project.js
@@ -1,30 +1,41 @@
 import projectStyles from '../styles/Project.module.css';
-import Nav from './Nav';
 
 const Project = () => {
     return (
         <div className={projectStyles.container}>       
-            <br /><br /><br /><br /> 
+            <br /><br /><br /><br /><br /><br />  
             <div className={projectStyles.projectContent}>
-                <h1 className={projectStyles.projTitle}>Projects</h1>
+                <span className={projectStyles.projTitle}>Projects</span>
                 <div className={projectStyles.projCardDiv}>
                     <div className={projectStyles.projCard}>
                         <span className={projectStyles.projCardTitle}>p1</span>
+                        <br />
+                        <span className={projectStyles.projCardDesc}>This is a project.</span>
                     </div>
                     <div className={projectStyles.projCard}>
                         <span className={projectStyles.projCardTitle}>p2</span>
+                        <br />
+                        <span className={projectStyles.projCardDesc}>This is a project.</span>
                     </div>
                     <div className={projectStyles.projCard}>
                         <span className={projectStyles.projCardTitle}>p3</span>
+                        <br />
+                        <span className={projectStyles.projCardDesc}>This is a project.</span>
                     </div>
                     <div className={projectStyles.projCard}>
                         <span className={projectStyles.projCardTitle}>p4</span>
+                        <br />
+                        <span className={projectStyles.projCardDesc}>This is a project.</span>
                     </div>
                     <div className={projectStyles.projCard}>
                         <span className={projectStyles.projCardTitle}>p5</span>
+                        <br />
+                        <span className={projectStyles.projCardDesc}>This is a project.</span>
                     </div>
                     <div className={projectStyles.projCard}>
                         <span className={projectStyles.projCardTitle}>p6</span>
+                        <br />
+                        <span className={projectStyles.projCardDesc}>This is a project.</span>
                     </div>
                 </div>
             </div>
