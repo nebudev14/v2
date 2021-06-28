0c7e5369339f8b4a73e0eb7f079d7ac9d9fd818a
warren yun <xxwarrenhdxx@gmail.com>
2021-06-27 21:32:42 -0400

    Add card templates

--- components/Project.js
+++ components/Project.js
@@ -8,7 +8,24 @@ const Project = () => {
             <div className={projectStyles.projectContent}>
                 <h1 className={projectStyles.projTitle}>Projects</h1>
                 <div className={projectStyles.projCardDiv}>
-                    
+                    <div className={projectStyles.projCard}>
+                        <span className={projectStyles.projCardTitle}>p1</span>
+                    </div>
+                    <div className={projectStyles.projCard}>
+                        <span className={projectStyles.projCardTitle}>p2</span>
+                    </div>
+                    <div className={projectStyles.projCard}>
+                        <span className={projectStyles.projCardTitle}>p3</span>
+                    </div>
+                    <div className={projectStyles.projCard}>
+                        <span className={projectStyles.projCardTitle}>p4</span>
+                    </div>
+                    <div className={projectStyles.projCard}>
+                        <span className={projectStyles.projCardTitle}>p5</span>
+                    </div>
+                    <div className={projectStyles.projCard}>
+                        <span className={projectStyles.projCardTitle}>p6</span>
+                    </div>
                 </div>
             </div>
         </div>
