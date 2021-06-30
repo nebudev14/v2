d3af78e6896e30cba8773969414c6a0e1d516701
warren yun <xxwarrenhdxx@gmail.com>
2021-06-30 07:31:51 -0400

    Style cards on hover

--- components/Project.js
+++ components/Project.js
@@ -8,72 +8,84 @@ const Project = () => {
                     <strong>Projects</strong>
                 </span>
                 <div className={projectStyles.projCardDiv}>
-                    <div className={projectStyles.projCard}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    <a href="https://github.com/Rona-Radar/app" target="_blank">Rona Radar</a>
-                                </strong>
-                            </span>
-                            <br /><br />
-                            <span className={projectStyles.projCardDesc}>This is a project.</span>
+                    <a href="https://github.com/Rona-Radar/app" target="_blank" className={projectStyles.projLink}>    
+                        <div className={projectStyles.projCard}>
+                            <div className={projectStyles.projBox}>
+                                <span className={projectStyles.projCardTitle}>
+                                    <strong>
+                                        Rona Radar
+                                    </strong>
+                                </span>
+                                <br /><br />
+                                <span className={projectStyles.projCardDesc}>This is a project.</span>
+                            </div>
                         </div>
-                    </div>
-                    <div className={projectStyles.projCard}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    <a href="https://github.com/NebuDev14/TheraTracker" target="_blank">Thera Tracker</a>
-                                </strong>
-                            </span>
-                            <br /><br />
-                            <span className={projectStyles.projCardDesc}>This is a project.</span>
+                    </a>
+                    <a href="https://github.com/NebuDev14/TheraTracker" target="_blank" className={projectStyles.projLink}>
+                        <div className={projectStyles.projCard}>
+                            <div className={projectStyles.projBox}>
+                                <span className={projectStyles.projCardTitle}>
+                                    <strong>
+                                        Thera Tracker
+                                    </strong>
+                                </span>
+                                <br /><br />
+                                <span className={projectStyles.projCardDesc}>This is a project.</span>
+                            </div>
                         </div>
-                    </div>
-                    <div className={projectStyles.projCard}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    <a href="https://www.youtube.com/watch?v=1FFBsX5C61Q" target="_blank">W.I.P.</a>
-                                </strong>
-                            </span>
-                            <br /><br />
-                            <span className={projectStyles.projCardDesc}>Check back soon</span>
+                     </a>
+                    <a href="https://www.youtube.com/watch?v=1FFBsX5C61Q" target="_blank" className={projectStyles.projLink}>
+                        <div className={projectStyles.projCard}>
+                            <div className={projectStyles.projBox}>
+                                <span className={projectStyles.projCardTitle}>
+                                    <strong>
+                                        W.I.P.
+                                    </strong>
+                                </span>
+                                <br /><br />
+                                <span className={projectStyles.projCardDesc}>Check back soon</span>
+                            </div>
                         </div>
-                    </div>
-                    <div className={projectStyles.projCard}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    <a href="https://github.com/NebuDev14/G.O.A.T-Notes" target="_blank">G.O.A.T Notes</a>
-                                </strong>
-                            </span>
-                            <br /><br />
-                            <span className={projectStyles.projCardDesc}>This is a project.</span>
+                    </a>
+                    <a href="https://github.com/NebuDev14/G.O.A.T-Notes" target="_blank" className={projectStyles.projLink}>
+                        <div className={projectStyles.projCard}>
+                            <div className={projectStyles.projBox}>
+                                <span className={projectStyles.projCardTitle}>
+                                    <strong>
+                                        G.O.A.T Notes
+                                    </strong>
+                                </span>
+                                <br /><br />
+                                <span className={projectStyles.projCardDesc}>This is a project.</span>
+                            </div>
                         </div>
-                    </div>
-                    <div className={projectStyles.projCard}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    <a href="https://midnighthacks.tech/" target="_blank">Midnight Hacks</a>
-                                </strong>
-                            </span>
-                            <br /><br />
-                            <span className={projectStyles.projCardDesc}>This is a project.</span>
+                    </a>
+                    <a href="https://midnighthacks.tech/" target="_blank" className={projectStyles.projLink}>
+                        <div className={projectStyles.projCard}>
+                            <div className={projectStyles.projBox}>
+                                <span className={projectStyles.projCardTitle}>
+                                    <strong>
+                                        Midnight Hacks
+                                    </strong>
+                                </span>
+                                <br /><br />
+                                <span className={projectStyles.projCardDesc}>This is a project.</span>
+                            </div>
                         </div>
-                    </div>
-                    <div className={projectStyles.projCard}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">W.I.P.</a>
-                                </strong>
-                            </span>
-                            <br /><br />
-                            <span className={projectStyles.projCardDesc}>Check back soon</span>
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" className={projectStyles.projLink}>
+                        <div className={projectStyles.projCard}>
+                            <div className={projectStyles.projBox}>
+                                <span className={projectStyles.projCardTitle}>
+                                    <strong>
+                                        W.I.P.
+                                    </strong>
+                                </span>
+                                <br /><br />
+                                <span className={projectStyles.projCardDesc}>Check back soon</span>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
