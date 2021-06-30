7f64f64aa399a627f69995472915ed3a3998b2f6
warren yun <xxwarrenhdxx@gmail.com>
2021-06-30 07:56:54 -0400

    Fix massive href bug for desktop

--- components/Project.js
+++ components/Project.js
@@ -8,47 +8,51 @@ const Project = () => {
                     <strong>Projects</strong>
                 </span>
                 <div className={projectStyles.projCardDiv}>
-                    <a href="https://github.com/Rona-Radar/app" target="_blank" className={projectStyles.projLink}>    
-                        <div className={projectStyles.projCard}>
-                            <div className={projectStyles.projBox}>
-                                <span className={projectStyles.projCardTitle}>
-                                    <strong>
-                                        Rona Radar
-                                    </strong>
-                                </span>
-                                <br /><br />
-                                <span className={projectStyles.projCardDesc}>This is a project.</span>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="https://github.com/NebuDev14/TheraTracker" target="_blank" className={projectStyles.projLink}>
-                        <div className={projectStyles.projCard}>
-                            <div className={projectStyles.projBox}>
-                                <span className={projectStyles.projCardTitle}>
-                                    <strong>
-                                        Thera Tracker
-                                    </strong>
-                                </span>
-                                <br /><br />
-                                <span className={projectStyles.projCardDesc}>This is a project.</span>
-                            </div>
-                        </div>
-                     </a>
-                    <a href="https://www.youtube.com/watch?v=1FFBsX5C61Q" target="_blank" className={projectStyles.projLink}>
-                        <div className={projectStyles.projCard}>
-                            <div className={projectStyles.projBox}>
-                                <span className={projectStyles.projCardTitle}>
-                                    <strong>
-                                        W.I.P.
-                                    </strong>
-                                </span>
-                                <br /><br />
-                                <span className={projectStyles.projCardDesc}>Check back soon</span>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="https://github.com/NebuDev14/G.O.A.T-Notes" target="_blank" className={projectStyles.projLink}>
-                        <div className={projectStyles.projCard}>
+                    <div className={projectStyles.projCard}>
+                        <a href="https://github.com/Rona-Radar/app" target="_blank" className={projectStyles.projLink}>    
+                                <div className={projectStyles.projBox}>
+                                    <span className={projectStyles.projCardTitle}>
+                                        <strong>
+                                            Rona Radar
+                                        </strong>
+                                    </span>
+                                    <br /><br />
+                                    <span className={projectStyles.projCardDesc}>This is a project.</span>
+                                </div>
+                        </a>
+                    </div>
+                    <div className={projectStyles.projCard}>
+                        <a href="https://github.com/NebuDev14/TheraTracker" target="_blank" className={projectStyles.projLink}>
+                            
+                                <div className={projectStyles.projBox}>
+                                    <span className={projectStyles.projCardTitle}>
+                                        <strong>
+                                            Thera Tracker
+                                        </strong>
+                                    </span>
+                                    <br /><br />
+                                    <span className={projectStyles.projCardDesc}>This is a project.</span>
+                                </div>
+                            
+                        </a>
+                    </div>
+                    <div className={projectStyles.projCard}>
+                        <a href="https://www.youtube.com/watch?v=1FFBsX5C61Q" target="_blank" className={projectStyles.projLink}>
+                            
+                                <div className={projectStyles.projBox}>
+                                    <span className={projectStyles.projCardTitle}>
+                                        <strong>
+                                            W.I.P.
+                                        </strong>
+                                    </span>
+                                    <br /><br />
+                                    <span className={projectStyles.projCardDesc}>Check back soon</span>
+                                </div>
+                            
+                        </a>
+                    </div>
+                    <div className={projectStyles.projCard}>
+                        <a href="https://github.com/NebuDev14/G.O.A.T-Notes" target="_blank" className={projectStyles.projLink}>
                             <div className={projectStyles.projBox}>
                                 <span className={projectStyles.projCardTitle}>
                                     <strong>
@@ -58,34 +62,37 @@ const Project = () => {
                                 <br /><br />
                                 <span className={projectStyles.projCardDesc}>This is a project.</span>
                             </div>
-                        </div>
-                    </a>
-                    <a href="https://midnighthacks.tech/" target="_blank" className={projectStyles.projLink}>
-                        <div className={projectStyles.projCard}>
-                            <div className={projectStyles.projBox}>
-                                <span className={projectStyles.projCardTitle}>
-                                    <strong>
-                                        Midnight Hacks
-                                    </strong>
-                                </span>
-                                <br /><br />
-                                <span className={projectStyles.projCardDesc}>This is a project.</span>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" className={projectStyles.projLink}>
-                        <div className={projectStyles.projCard}>
-                            <div className={projectStyles.projBox}>
-                                <span className={projectStyles.projCardTitle}>
-                                    <strong>
-                                        W.I.P.
-                                    </strong>
-                                </span>
-                                <br /><br />
-                                <span className={projectStyles.projCardDesc}>Check back soon</span>
-                            </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
+                    <div className={projectStyles.projCard}>
+                        <a href="https://midnighthacks.tech/" target="_blank" className={projectStyles.projLink}>
+                            
+                                <div className={projectStyles.projBox}>
+                                    <span className={projectStyles.projCardTitle}>
+                                        <strong>
+                                            Midnight Hacks
+                                        </strong>
+                                    </span>
+                                    <br /><br />
+                                    <span className={projectStyles.projCardDesc}>This is a project.</span>
+                                </div>
+                        </a>
+                    </div>
+                    <div className={projectStyles.projCard}>
+                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" className={projectStyles.projLink}>
+                            
+                                <div className={projectStyles.projBox}>
+                                    <span className={projectStyles.projCardTitle}>
+                                        <strong>
+                                            W.I.P.
+                                        </strong>
+                                    </span>
+                                    <br /><br />
+                                    <span className={projectStyles.projCardDesc}>Check back soon</span>
+                                </div>
+                            
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
