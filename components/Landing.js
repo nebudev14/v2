cee91b656d1c5712d6c49f06522d5ad11be1720f
warren yun <xxwarrenhdxx@gmail.com>
2021-07-02 09:48:17 -0400

    Add webring

--- components/Landing.js
+++ components/Landing.js
@@ -12,17 +12,23 @@ const Landing = () => {
                 <h2 className={landingStyles.text}>
                     a high schooler with a passion for programming and a strong addiction to learning new things
                 </h2>
-            </div>
-            <div className={landingStyles.socialMediaWrapper}>
-                <a href="https://www.instagram.com/nebudev14/" target="_blank">
-                    <img src="/instagram.svg" className={landingStyles.logo} />
-                </a>
-                <a href="https://twitter.com/nebudev14" target="_blank">
-                    <img src="/twitter.svg" className={landingStyles.logo} />
-                </a>
-                <a href="https://github.com/NebuDev14" target="_blank">
-                    <img src="/github.svg" className={landingStyles.logo} />
-                </a>
+                <div id="webring-wrapper" className={landingStyles.webringWrapper}>
+                    <a href="https://webring.hackclub.com/" id="previousBtn" class={landingStyles.webringAnchor} title="Previous">‹</a>
+                    <a href="https://webring.hackclub.com/" class={landingStyles.webringLogo} title="Hack Club Webring" alt="Hack Club Webring"></a>
+                    <a href="https://webring.hackclub.com/" id="nextBtn" class={landingStyles.webringAnchor} title="Next">›</a>
+                    <script src="https://webring.hackclub.com/public/embed.min.js"></script>
+                </div>
+                <div className={landingStyles.socialMediaWrapper}>
+                    <a href="https://www.instagram.com/nebudev14/" target="_blank">
+                        <img src="/instagram.svg" className={landingStyles.logo} title="Instagram" alt="Instagram" />
+                    </a>
+                    <a href="https://twitter.com/nebudev14" target="_blank">
+                        <img src="/twitter.svg" className={landingStyles.logo} title="Twitter" alt="Twitter" />
+                    </a>
+                    <a href="https://github.com/NebuDev14" target="_blank">
+                        <img src="/github.svg" className={landingStyles.logo} title="GitHub" alt="GitHub" />
+                    </a>
+                </div>
             </div>
         </div>
     );
