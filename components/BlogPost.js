2e729b14f4e8a938c4249bcbed5000e5bbadcdc5
warren yun <xxwarrenhdxx@gmail.com>
2021-07-07 17:55:37 -0400

    Finish blog post component

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,9 +1,17 @@
 import Link from 'next/link';
+import blogPostStyles from '../styles/BlogPost.module.css';
 
 function BlogPost({blog}) {
     return (
-        <div>
-
+        <div className={blogPostStyles.blogPost}>
+            <div>
+                <img src={blog.img} />
+                <div className={blogPostStyles.date}>{blog.date}</div>
+                <Link href={`/posts/${blog.slug}`}>
+                    <a className={blogPostStyles.blogTitle}>{blog.title}</a>
+                </Link>
+                <p>{blog.description}</p>
+            </div>
         </div>
     );
 }
