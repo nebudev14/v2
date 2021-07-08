cdd252d62062c803c5a82b8144d09c55633a0f44
warren yun <xxwarrenhdxx@gmail.com>
2021-07-08 12:48:12 -0400

    Get blog content

--- pages/posts/[blog].js
+++ pages/posts/[blog].js
@@ -1,5 +1,6 @@
 import matter from "gray-matter";
 import ReactMarkdown from "react-markdown";
+import blogStyles from '../../styles/Blog/Blog.module.css';
 
 function Blog(props) {
     return (
@@ -7,6 +8,18 @@ function Blog(props) {
         
         </div>
     );
+}
+
+export const getServerSideProps = async context => {
+    const fs = require("fs");
+    const {blog} = context.params; 
+    const content = fs.readFileSync(`${process.cwd()}/content/${blog}.md`, 'utf-8');
+
+    return {
+        props: {
+            content
+        }
+    };
 }
 
 export default Blog;
