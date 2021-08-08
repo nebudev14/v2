13a85857459add705fb66503b2f06275d6dca43f
warren yun <xxwarrenhdxx@gmail.com>
2021-08-07 20:40:55 -0400

    Finish project component

--- components/Project.js
+++ components/Project.js
@@ -1,9 +1,18 @@
 import projectStyles from '../styles/Project.module.css';
 
-const Project = () => {
+function Project({details}) {
     return (
-        <div className={projectStyles.container}>       
-            
+        <div className={projectStyles.projCard}>
+            <a href={details.link} target="_blank" className={projectStyles.projLink}>    
+                <div className={projectStyles.projBox}>
+                    <span className={projectStyles.projCardTitle}>
+                        <strong>
+                            {details.name}
+                        </strong>
+                    </span>
+                    <span className={projectStyles.projCardDesc}>{details.desc}</span>
+                </div>
+            </a>
         </div>
     );
 }
