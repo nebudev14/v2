3dc1db40d81532c99d3aff4b61a363b8993e253d
warren yun <xxwarrenhdxx@gmail.com>
2021-08-07 21:29:36 -0400

    Map project component and remove hard code

--- pages/projects.js
+++ pages/projects.js
@@ -3,6 +3,39 @@ import Project from '../components/Project';
 
 import projectStyles from '../styles/Project.module.css';
 
+const projects = [
+    {
+        "name": "Rona Radar",
+        "link": "https://github.com/Rona-Radar/app",
+        "desc": "This is a project."
+    },
+    {
+        "name": "Thera Tracker",
+        "link": "https://github.com/NebuDev14/TheraTracker",
+        "desc": "This is a project."
+    },
+    {
+        "name": "W.I.P.",
+        "link": "https://www.youtube.com/watch?v=1FFBsX5C61Q",
+        "desc": "Check back soon!"
+    },
+    {
+        "name": "G.O.A.T Notes",
+        "link": "https://github.com/NebuDev14/G.O.A.T-Notes",
+        "desc": "This is a project."
+    },
+    {
+        "name": "Midnight Hacks",
+        "link": "https://midnighthacks.tech/",
+        "desc": "This is a project."
+    },
+    {
+        "name": "W.I.P.",
+        "link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "desc": "Check back soon!"
+    },
+];
+
 export default function ProjectDisplay() {
   return (
     <div>
@@ -14,79 +47,9 @@ export default function ProjectDisplay() {
                 <strong>Projects</strong>
             </span>
             <div className={projectStyles.projCardDiv}>
-                <div className={projectStyles.projCard}>
-                    <a href="https://github.com/Rona-Radar/app" target="_blank" className={projectStyles.projLink}>    
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    Rona Radar
-                                </strong>
-                            </span>
-                            <span className={projectStyles.projCardDesc}>This is a project.</span>
-                        </div>
-                    </a>
-                </div>
-                <div className={projectStyles.projCard}>
-                    <a href="https://github.com/NebuDev14/TheraTracker" target="_blank" className={projectStyles.projLink}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    Thera Tracker
-                                </strong>
-                            </span>
-                            <span className={projectStyles.projCardDesc}>This is a project.</span>
-                        </div>
-                    </a>
-                </div>
-                <div className={projectStyles.projCard}>
-                    <a href="https://www.youtube.com/watch?v=1FFBsX5C61Q" target="_blank" className={projectStyles.projLink}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    W.I.P.
-                                </strong>
-                            </span>
-                                
-                            <span className={projectStyles.projCardDesc}>Check back soon</span>
-                        </div>
-                    </a>
-                </div>
-                <div className={projectStyles.projCard}>
-                    <a href="https://github.com/NebuDev14/G.O.A.T-Notes" target="_blank" className={projectStyles.projLink}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    G.O.A.T Notes
-                                </strong>
-                            </span>
-                            <span className={projectStyles.projCardDesc}>This is a project.</span>
-                        </div>
-                    </a>
-                </div>
-                <div className={projectStyles.projCard}>
-                    <a href="https://midnighthacks.tech/" target="_blank" className={projectStyles.projLink}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    Midnight Hacks
-                                </strong>
-                            </span>
-                            <span className={projectStyles.projCardDesc}>This is a project.</span>
-                        </div>
-                    </a>
-                </div>
-                <div className={projectStyles.projCard}>
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" className={projectStyles.projLink}>
-                        <div className={projectStyles.projBox}>
-                            <span className={projectStyles.projCardTitle}>
-                                <strong>
-                                    W.I.P.
-                                </strong>
-                            </span>
-                            <span className={projectStyles.projCardDesc}>Check back soon</span>
-                        </div>
-                    </a>
-                </div>
+                {projects.map((project, i) => (
+                    <Project details={project} key={i} />
+                ))}
             </div>
         </div>
     </div>
